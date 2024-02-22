0d724c5ebbb6f96a906e301ec3e83d4313fc1766
MduduziSB <sibusisom.skhumkane@gmail.com>
2024-02-22 14:25:18 +0000

    Task 19

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -53,5 +53,40 @@ def logout() -> str:
     abort(403)
 
 
+@app.route('/profile', methods=['POST'])
+def profile() -> str:
+    """ responds to the GET /profile route """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if not user:
+        abort(403)
+    return jsonify({"email": user.email}), 200
+
+
+@app.route('/reset_password', methods=['POST'])
+def get_reset_password_token() -> str:
+    """ Function to respond to the POST /reset_password route """
+    email = request.form.get("email")
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token})
+    except ValueError:
+        abort(403)
+
+
+@app.route('/reset_password', methods=['PUT'])
+def update_password():
+    """ function in the app module to respond to the PUT /reset_password """
+    user_email = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    password = request.form.get("new_password")
+    try:
+        AUTH.update_password(reset_token, password)
+        return jsonify({"email": user_email, "message": "Password updated"})
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
